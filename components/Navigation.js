2e3a172085bff2967f984382da42cd46c201c59c
kaloyanTry <kaloyantryavna@gmail.com>
2024-05-31 15:04:34 +0300

    Added Middleware in Next.js project

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { auth } from '@/lib/auth';
 
-const Navigation = () => {
+const Navigation = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex gap-16'>
@@ -21,17 +25,32 @@ const Navigation = () => {
           </Link>
         </li>
         <li>
-          <Link
-            href='/account'
-            className='hover:text-accent-400 transition-colors'
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors flex items-center gap-4'
+            >
+              <img
+                className='h-8 rounded-full'
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy='no-referrer'
+              />
+              <span> Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors'
+            >
+              Guest area
+            </Link>
+          )}
         </li>
         <li>
           <Link
             href='/users'
-            className='hover:text-accent-400 transition-colors'
+            className='hover:text-accent-400 transition-colors '
           >
             Oasis Users
           </Link>
--- components/Reservation.js
+++ components/Reservation.js
@@ -1,12 +1,15 @@
 import { getBookedDatesByCabinId, getSettings } from '@/lib/data-service';
+import { auth } from '@/lib/auth';
 import DateSelector from './DateSelector';
 import ReservationForm from './ReservationForm';
+import LoginMessage from './LoginMessage';
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
 
   return (
     <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
@@ -15,7 +18,11 @@ async function Reservation({ cabin }) {
         settings={settings}
         bookedDates={bookedDates}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
